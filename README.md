0654dd93b37b5a6ad345c933c9dd7f36f420927c
Julia Bekker <bekker_yulya@mail.ru>
2024-03-26 23:12:47 +0300

    step 7 26/03

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 <a href="https://codeclimate.com/github/bekkerjul/python-project-49/maintainability"><img src="https://api.codeclimate.com/v1/badges/c595224a6a3a4ae11716/maintainability" /></a>
 <a href="https://asciinema.org/a/Eq9UiWfr6l7hUQoVQmx2k8c1O" target="_blank"><img src="https://asciinema.org/a/Eq9UiWfr6l7hUQoVQmx2k8c1O.svg" /></a>
 <a href="https://asciinema.org/a/2dBVaN502yqJQYJnBoPdkeCNi" target="_blank"><img src="https://asciinema.org/a/2dBVaN502yqJQYJnBoPdkeCNi.svg" /></a>
+<a href="https://asciinema.org/a/o2nK1aweVotwthXvhqAIC1vYE" target="_blank"><img src="https://asciinema.org/a/o2nK1aweVotwthXvhqAIC1vYE.svg" /></a>
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,5 +1,38 @@
 from .brain_games import welcome_game
 from ..cly import welcome_user
+from random import randint
+from .logic_game import logic
+
+
+def find_gcd(num):
+    gcds = set()
+    for elem in range(1, num + 1):
+        if elem % elem == 0:
+            gcds.add(elem)
+    return gcds
+
 
 def brain_gcd():
-    
+    name = welcome_game()
+    points = 0
+    question = 'Find the greatest common divisor of given numbers.'
+
+    while points < 3:
+        num1 = randint(1, 10)
+        num2 = randint(1, 10)
+        print(f'Question: {num1} {num2}')
+        correct_answer = max(list(find_gcd(num1) & find_gcd(num2)))
+        flag = logic(question, correct_answer, points, name)
+        if flag == True:
+            points += 1
+        else:
+            print(flag)
+
+    print(f'Congratulations, {name}')
+
+
+def main():
+    brain_gcd()
+
+if __name__ == '__main__':
+    main()
