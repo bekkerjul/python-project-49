75a4287915407253b76e700759c9dca1b2fd5cdc
Julia Bekker <bekker_yulya@mail.ru>
2024-04-10 23:49:27 +0300

    update after tests 10/04 part 4

--- brain_games/scripts/logic_game.py
+++ brain_games/scripts/logic_game.py
@@ -1,7 +1,7 @@
 def logic(question, correct_answer, points, name):
     print(question)
     answer = input()
-    if answer.isdigit():
+    if answer.isdigit() or answer[1:].isdigit():
         answer = int(answer)
     if answer == correct_answer:
         return True
