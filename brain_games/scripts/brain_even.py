8cb6061cb1cd5c5f289df95b1b76c1a70b2f9449
Julia Bekker <bekker_yulya@mail.ru>
2024-04-15 21:37:13 +0300

    update script brain-even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -6,27 +6,18 @@ def is_even():
     num = randint(0, 10000000)
     print(f'Question: {num}')
     answer = int(input('Your answer: '))
-    return 'yes' if answer % 2 == 0 else 'no', answer
+    correct_answer = num % 2
+
+    if (correct_answer == 1 and answer == 'no') or (correct_answer == 0 and answer == 'yes'):
+        return True, answer, correct_answer
+    else:
+        return False,answer, correct_answer
 
 
 def even_game():
     question = 'Answer "yes" if the number is even, otherwise answer "no".'
-    is_even()
-
-        num = randint(0, 10000000)
-        print(f'Question: {num}')
-        answer = input('Your answer: ')
-        right_answer = is_even(num)
-        if answer == right_answer:
-            print('Correct!')
-            points += 1
-        else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'.")
-            print(f"Let's try again, {name}!")
-            break
-
-    if points == 3:
-        print(f'Congratulations, {name}!')
+    flag, answer, correct_answer = is_even()
+    return question, flag, answer, correct_answer
 
 
 def main():
