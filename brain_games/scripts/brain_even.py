dd9b3b2477a543dbffa4c05e730d7ce7e08c7948
Julia Bekker <bekker_yulya@mail.ru>
2024-04-16 19:53:05 +0300

    16/04 update logic

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,23 +1,18 @@
 from random import randint
-from ..cly import welcome_user
-
-
-def is_even():
-    num = randint(0, 10000000)
-    print(f'Question: {num}')
-    answer = int(input('Your answer: '))
-    correct_answer = num % 2
-
-    if (correct_answer == 1 and answer == 'no') or (correct_answer == 0 and answer == 'yes'):
-        return True, answer, correct_answer
-    else:
-        return False,answer, correct_answer
 
 
 def even_game():
     question = 'Answer "yes" if the number is even, otherwise answer "no".'
-    flag, answer, correct_answer = is_even()
-    return question, flag, answer, correct_answer
+    num = randint(0, 10000000)
+    answer = int(input('Your answer: '))
+    correct_answer = num % 2
+
+    if (correct_answer == 1 and answer == 'no') or (correct_answer == 0 and answer == 'yes'):
+        flag = True
+    else:
+        flag = False
+
+    return [question, flag]
 
 
 def main():
