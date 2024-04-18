1e23ce62e9b3af6327d04e356982972d309ba184
Julia Bekker <bekker_yulya@mail.ru>
2024-04-19 00:04:58 +0300

    update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,7 +5,9 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def generate_round():
     question = randint(1, 100)
-    gsd = len([i for i in range(1, question + 1) if question % i == 0 else False])
-    answer = 'yes' if gsd == 2 else 'no'
+    dividers = []
+    for num in range(1, question + 1):
+        dividers.append(num) if question % num == 0 else False
+    answer = 'yes' if len(dividers) == 2 else 'no'
     return str(question), answer
 
