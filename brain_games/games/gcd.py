23840f292474918866a7f326ba8bbb1595db8202
Julia Bekker <bekker_yulya@mail.ru>
2024-04-18 23:34:48 +0300

    update gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -3,13 +3,17 @@ from random import randint
 DESCRIPTION = 'Find the greatest common divisor of given numbers.'
 
 
+def find_divisors(num):
+    divisors_list = []
+    for n in range(1, num + 1):
+        divisors_list.append(n) if num % 2 == 0 else False
+    return set(divisors_list)
+
+
 def generate_round():
     num1 = randint(1, 10)
     num2 = randint(1, 10)
     question = f'{num1} {num2}'
-    gsd1 = set([i for i in range(num1) if num % i == 0 else 1])
-    gsd2 = set([i for i in range(num2) if num % i == 0 else 1])
-    answer = max(list(find_gcd(num1) & find_gcd(num2)))
+    answer = max(list(find_divisors(num1) & find_divisors(num2)))
     return question, str(answer)
-
 
